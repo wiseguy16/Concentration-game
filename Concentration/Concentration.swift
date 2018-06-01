9ab43921b8dcd30c28e91cfefe545bef568c4653
Greg Weiss <gergusa04@gmail.com>
2018-06-01 18:24:53 -0400

    flip button function added

---
+++ Concentration/Concentration.swift
@@ -0,0 +1,13 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Greg Weiss on 6/1/18.
+//  Copyright © 2018 Greg Weiss. All rights reserved.
+//
+
+import Foundation
+
+class Concentration {
+    
+}
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle ==  emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
+
+
 
 
 }
