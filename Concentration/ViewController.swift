b66c27d952a1612d97d55b6d253a66feace8c505
Greg Weiss <gergusa04@gmail.com>
2018-06-02 20:27:26 -0400

    added counter label

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +27,7 @@ class ViewController: UIViewController {
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
     }
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
